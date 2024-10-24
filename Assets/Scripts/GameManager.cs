9492f9105676b33aa85bd4761b76fb2b7083ef75
박재형 <only1991@naver.com>
2024-10-24 16:02:31 +0900

    [GameManager] bounds, Tag, score 관리 부분 추가

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,8 +4,30 @@ using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
-    void Start()
-    {
+    [SerializeField] private GameObject background;
+    [SerializeField] private GameObject playground;
 
+    public enum Tag { Player, Wall, Bullet, DestroyArea, HealthPotion, Wisp };
+    [HideInInspector] public Bounds allBounds;
+    [HideInInspector] public Bounds playBounds;
+
+    private int score;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        allBounds = background.GetComponent<Collider2D>().bounds;
+        playBounds = playground.GetComponent<Collider2D>().bounds;
+    }
+
+    public void AddScore(int score)
+    {
+        this.score += score;
+    }
+
+    public void GameOver()
+    {
+        Debug.Log("Game Over!!");
     }
 }
